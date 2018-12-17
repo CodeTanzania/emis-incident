a42009556e3cd4ce984bd183f048b3231e50c69c
lykmapipo <lallyelias87@gmail.com>
2018-12-17 16:45:50 +0300

    Improve package main export jsdocs

--- index.js
+++ index.js
@@ -23,61 +23,115 @@
 
 
 /* dependencies */
-const path = require('path');
 const _ = require('lodash');
+const { include } = require('@lykmapipo/include');
 const app = require('@lykmapipo/express-common');
-const mongoose = require('mongoose');
-require('mongoose-schema-jsonschema')(mongoose);
+const pkg = include(__dirname, 'package.json');
+const Incident = include(__dirname, 'lib', 'incident.model');
+const Action = include(__dirname, 'lib', 'action.model');
+const Task = include(__dirname, 'lib', 'task.model');
+const incidentRouter = include(__dirname, 'lib', 'incident.http.router');
+const actionRouter = include(__dirname, 'lib', 'action.http.router');
+const taskRouter = include(__dirname, 'lib', 'task.http.router');
 
 
-/* declarations */
-const pkg = require(path.join(__dirname, 'package.json'));
-const fields = [
-  'name',
-  'description',
-  'version',
-  'license',
-  'homepage',
-  'repository',
-  'bugs',
-  'sandbox',
-  'contributors'
-];
+/**
+ * @name info
+ * @description package information
+ * @type {Object}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 1.0.0
+ * @version 0.1.0
+ */
+exports.info = _.merge({}, _.pick(pkg, [
+  'name', 'description', 'version', 'license',
+  'homepage', 'repository', 'bugs', 'sandbox', 'contributors'
+]));
 
 
-/* extract information from package.json */
-const info = _.merge({}, _.pick(pkg, fields));
-
-
-/* export package(module) info */
-exports.info = info;
-
-
-/* import models */
-const Incident = require(path.join(__dirname, 'lib', 'incident.model'));
-const Action = require(path.join(__dirname, 'lib', 'action.model'));
-const Task = require(path.join(__dirname, 'lib', 'task.model'));
-
-
-/* export models */
+/**
+ * @name Incident
+ * @description Incident model
+ * @type {mongoose.Model}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 1.0.0
+ * @version 0.1.0
+ */
 exports.Incident = Incident;
+
+
+/**
+ * @name Action
+ * @description Action model
+ * @type {mongoose.Model}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 1.0.0
+ * @version 0.1.0
+ */
 exports.Action = Action;
+
+
+/**
+ * @name Task
+ * @description Task model
+ * @type {mongoose.Model}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 1.0.0
+ * @version 0.1.0
+ */
 exports.Task = Task;
 
 
-/* import routers */
-const incidentRouter =
-  require(path.join(__dirname, 'lib', 'incident.http.router'));
-const actionRouter =
-  require(path.join(__dirname, 'lib', 'action.http.router'));
-const taskRouter =
-  require(path.join(__dirname, 'lib', 'task.http.router'));
-
-
-/* export incident router */
+/**
+ * @name incidentRouter
+ * @description incident http router
+ * @type {express.Router}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 1.0.0
+ * @version 0.1.0
+ */
 exports.incidentRouter = incidentRouter;
+
+
+/**
+ * @name actionRouter
+ * @description action http router
+ * @type {express.Router}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 1.0.0
+ * @version 0.1.0
+ */
 exports.actionRouter = actionRouter;
+
+
+/**
+ * @name taskRouter
+ * @description task http router
+ * @type {express.Router}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 1.0.0
+ * @version 0.1.0
+ */
 exports.taskRouter = taskRouter;
+
+
+/**
+ * @name apiVersion
+ * @description http router api version
+ * @type {String}
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 1.0.0
+ * @version 0.1.0
+ */
+exports.apiVersion = incidentRouter.apiVersion;
 
 
 /* export app */
