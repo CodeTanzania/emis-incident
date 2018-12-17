0f6a56d8bbd4c4e8b763e9abcb799027019d41fd
lykmapipo <lallyelias87@gmail.com>
2018-12-17 18:07:01 +0300

    Build latest apidoc

---
+++ docs/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "emis-incident",
+  "version": "1.0.0",
+  "description": "A representation of an entity which define and track an instance(or occurrence) of an emergency(or disaster) event.",
+  "title": "EMIS - Incident",
+  "url": "https://emis-incident.herokuapp.com/v1",
+  "sampleUrl": "https://emis-incident.herokuapp.com/v1",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-12-17T15:02:58.569Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
